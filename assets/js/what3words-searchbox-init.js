21972ba9e77da09e393a84660b775c0a80341e4c
Gaz Jessep <gareth@what3words>
2020-05-15 17:14:15 +0100

    Persist id & name on component's input so normal form label & submission works. WIP setting properties on component

--- assets/js/what3words-searchbox-init.js
+++ assets/js/what3words-searchbox-init.js
@@ -1,56 +1,51 @@
 
 if (typeof What3wordsSearchbox != "undefined") {
-    var country = What3wordsSearchbox.country_filter_selector !== '' ? What3wordsSearchbox.country_filter_selector : What3wordsSearchbox.country_code;
-    // $(What3wordsSearchbox.inputSelectors).w3wAddress({
-    //     debug: true,
-    //     key: What3wordsSearchbox.api_key,
-    //     items_to_show: What3wordsSearchbox.suggestions,
-    //     multilingual: What3wordsSearchbox.multilingual,
-    //     lang: What3wordsSearchbox.lang,
-    //     results: What3wordsSearchbox.suggestions,
-    //     country_filter: country,
-    //     direction: What3wordsSearchbox.text_direction,
-    //     placeholder: What3wordsSearchbox.input_placeholder,
-    //     validation: true
-    // });
 
     console.log('settings:\n',What3wordsSearchbox);
 
+    const w3wComponent = document.createElement('what3words-autosuggest',
+      { is: 'what3words-autosuggest' }
+    )
 
-    const w3wComponent = document.createElement('what3words-autosuggest', { is: 'what3words-autosuggest' })
-    const inputSelectors = document.querySelectorAll(`${What3wordsSearchbox.input_selectors}`)
+    if (What3wordsSearchbox.language) {
+      w3wComponent.language = What3wordsSearchbox.lang
+      w3wComponent.setAttribute('language', What3wordsSearchbox.lang)
+    }
+    if (What3wordsSearchbox.placeholder) {
+      w3wComponent.placeholder = What3wordsSearchbox.input_placeholder
+    }
+    console.log('w3wComponent', w3wComponent)
 
-    inputSelectors.forEach((targetInput) => {
-      const originalName = targetInput.name,
-            originalId   = targetInput.id
-      console.log(originalId, originalName)
+    const targetInputs = document.querySelectorAll(`${What3wordsSearchbox.input_selectors}`)
 
-      targetInput.parentNode.replaceChild(w3wComponent, targetInput)
-      const customInput = document.querySelector('[is="what3words-autosuggest"]').querySelector('input')
-      customInput.name = originalName
-      customInput.id = originalId
+    targetInputs.forEach((targetInput) => {
+
+      const originalName = targetInput.name || '',
+            originalId   = targetInput.id || ''
+
+      const targetParent = targetInput.parentNode
+
+      targetParent.replaceChild(w3wComponent, targetInput)
+
+
+      setTimeout(() => {
+        const componentInstance = targetParent.querySelector('[is="what3words-autosuggest"]')
+        const customInput =  componentInstance.querySelector('input')
+        if (originalName) { customInput.name = originalName }
+        if (originalId)   { customInput.id   = originalId   }
+
+        console.log('componentInstance', componentInstance, componentInstance.language, componentInstance.placeholder)
+
+        if (What3wordsSearchbox.language) {
+          componentInstance.setAttribute('language', What3wordsSearchbox.lang)
+          componentInstance.language = What3wordsSearchbox.lang
+        }
+        if (What3wordsSearchbox.placeholder) {
+          componentInstance.placeholder = What3wordsSearchbox.input_placeholder
+        }
+
+        console.log('componentInstance', componentInstance, componentInstance.language, componentInstance.placeholder)
+      },1)
     })
 
 }
-
-
-/*
-specimen:
-
-<what3words-autosuggest
-autosuggest-focus="51.4911,-0.2120"
-clip-to-bounding-box="51.4912,-0.2121,51.4911,-0.2120"
-clip-to-circle="51.4911,-0.2120,6"
-clip-to-country="AG"
-clip-to-polygon
-error-message="whoops!"
-language="pt"
-n-focus-results="3"
-n-results="5"
-placeholder="your.three.words"
-
-/>
-
-
-
-*/
