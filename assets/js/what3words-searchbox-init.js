35cbb9521e3c0eee0d3ee929e232f78899ec66b2
Gaz Jessep <gareth@what3words>
2020-05-18 14:35:19 +0100

    Apply settings (lang, placeholder) to component.

--- assets/js/what3words-searchbox-init.js
+++ assets/js/what3words-searchbox-init.js
@@ -1,50 +1,41 @@
 
 if (typeof What3wordsSearchbox != "undefined") {
 
-    console.log('settings:\n',What3wordsSearchbox);
-
+    //  console.log('settings:\n',What3wordsSearchbox);
     const w3wComponent = document.createElement('what3words-autosuggest',
       { is: 'what3words-autosuggest' }
     )
 
-    if (What3wordsSearchbox.language) {
-      w3wComponent.language = What3wordsSearchbox.lang
+    if (What3wordsSearchbox.lang) {
       w3wComponent.setAttribute('language', What3wordsSearchbox.lang)
     }
-    if (What3wordsSearchbox.placeholder) {
-      w3wComponent.placeholder = What3wordsSearchbox.input_placeholder
+    if (What3wordsSearchbox.input_placeholder) {
+      w3wComponent.setAttribute('placeholder', What3wordsSearchbox.input_placeholder)
     }
-    console.log('w3wComponent', w3wComponent)
 
     const targetInputs = document.querySelectorAll(`${What3wordsSearchbox.input_selectors}`)
 
     targetInputs.forEach((targetInput) => {
-
       const originalName = targetInput.name || '',
             originalId   = targetInput.id || ''
+            targetParent = targetInput.parentNode
 
-      const targetParent = targetInput.parentNode
+      const w3wComponentInstance = w3wComponent
+      w3wComponentInstance.setAttribute('data-input-orig-id', originalId)
+      w3wComponentInstance.setAttribute('data-input-orig-name', originalName)
+
 
       targetParent.replaceChild(w3wComponent, targetInput)
 
-
       setTimeout(() => {
-        const componentInstance = targetParent.querySelector('[is="what3words-autosuggest"]')
-        const customInput =  componentInstance.querySelector('input')
-        if (originalName) { customInput.name = originalName }
-        if (originalId)   { customInput.id   = originalId   }
+        //  FIX: this won't work if multiple sibling 
+        //  `What3wordsSearchbox.input_selectors`
+        // const componentInstance = targetParent.getElementsByTagName('what3words-autosuggest')[0]
+        // const customInput =  componentInstance.querySelector('input')
+        // if (originalName) { customInput.name = originalName }
+        // if (originalId)   { customInput.id   = originalId   }
 
-        console.log('componentInstance', componentInstance, componentInstance.language, componentInstance.placeholder)
-
-        if (What3wordsSearchbox.language) {
-          componentInstance.setAttribute('language', What3wordsSearchbox.lang)
-          componentInstance.language = What3wordsSearchbox.lang
-        }
-        if (What3wordsSearchbox.placeholder) {
-          componentInstance.placeholder = What3wordsSearchbox.input_placeholder
-        }
-
-        console.log('componentInstance', componentInstance, componentInstance.language, componentInstance.placeholder)
+      //  console.log('componentInstance', componentInstance, componentInstance.language, componentInstance.placeholder)
       },1)
     })
 
