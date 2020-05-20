366073c469b2735776cd1484b6fcc7103b123b0e
Gaz Jessep <gareth@what3words>
2020-05-20 15:09:31 +0100

    set readonly on hidden inputs

--- assets/js/what3words-searchbox-init.js
+++ assets/js/what3words-searchbox-init.js
@@ -1,35 +1,32 @@
 
 if (typeof What3wordsSearchbox != "undefined") {
 
-    console.log('settings:\n',What3wordsSearchbox)
+  console.log('settings:\n',What3wordsSearchbox)
 
-    const targetInputs = document.querySelectorAll(`${What3wordsSearchbox.input_selectors}`)
+  const targetInputs = document.querySelectorAll(`${What3wordsSearchbox.input_selectors}`)
 
-    targetInputs.forEach(function(targetInput) {
+  targetInputs.forEach(function(targetInput) {
 
-      const w3wComponent = document.createElement('what3words-autosuggest')
+    const w3wComponent = document.createElement('what3words-autosuggest')
+    const targetParent = targetInput.parentNode
 
-      if (What3wordsSearchbox.lang) {
-        w3wComponent.setAttribute('language', What3wordsSearchbox.lang)
+    if (What3wordsSearchbox.lang) {
+      w3wComponent.setAttribute('language', What3wordsSearchbox.lang)
+    }
+    if (What3wordsSearchbox.input_placeholder) {
+      w3wComponent.setAttribute('placeholder', What3wordsSearchbox.input_placeholder)
+    }
+
+    targetParent.insertBefore(w3wComponent, targetInput)
+    targetInput.style.display = 'none'
+    targetInput.setAttribute('readonly', true)
+    w3wComponent.addEventListener('valid', function(event) {
+      // if valid
+      if (event.detail) {
+        targetInput.value = event.target.value
+      } else {
+        targetInput.value = ''
       }
-      if (What3wordsSearchbox.input_placeholder) {
-        w3wComponent.setAttribute('placeholder', What3wordsSearchbox.input_placeholder)
-      }
-
-      const targetParent = targetInput.parentNode
-
-      targetParent.insertBefore(w3wComponent, targetInput)
-      targetInput.style.display = 'none'
-      w3wComponent.addEventListener('valid', function(event) {
-        // if valid
-        if (event.detail) {
-          targetInput.value = event.target.value
-        } else {
-          targetInput.value = ''
-        }
-      })
-
     })
-
-
+  })  //  forEach
 }
