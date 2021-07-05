9cd231f2465977aab8517458974386583773a886
Christian Shaw <cxs.shaw@googlemail.com>
2021-07-05 15:06:24 +0100

    chore(DB create in sql dump)

--- test/database/sql/wordpress-5.7.2.sql
+++ test/database/sql/wordpress-5.7.2.sql
@@ -17,6 +17,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE wordpress;
 USE wordpress;
 
 DROP TABLE IF EXISTS `wp_actionscheduler_actions`;
