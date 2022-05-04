cc572a49f199555f15d00dc3bdde098557dc5ff8
Christian Shaw <cxs.shaw@googlemail.com>
2022-05-04 16:15:58 +0100

    [mod] set DB on sql import

--- test/database/sql/wordpress-5.7.2-04052022.sql
+++ test/database/sql/wordpress-5.7.2-04052022.sql
@@ -17,6 +17,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+USE wordpress;
 
 DROP TABLE IF EXISTS `wp_actionscheduler_actions`;
 CREATE TABLE `wp_actionscheduler_actions` (
