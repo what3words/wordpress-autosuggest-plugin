e5de0a65a0af03e34553f8194165924273f0cf96
Frederick Lee <frederickrichard.lee@gmail.com>
2024-07-12 12:22:49 +0100

    tests: remove chaining in managed cypress test

--- test/cypress/support/index.ts
+++ test/cypress/support/index.ts
@@ -142,7 +142,10 @@ Cypress.Commands.add('completeCheckoutForm', (
   if (hasSeparate3waField) {
     cy.get(`${fieldPrefix}address_1`).focus().clear().type(address)
 
-    cy.get(isBilling ? '#w3w-billing' : '#w3w-shipping').scrollIntoView().click().clear().type(hint)
+    cy.get(isBilling ? '#w3w-billing' : '#w3w-shipping').scrollIntoView();
+    cy.get(isBilling ? '#w3w-billing' : '#w3w-shipping').click();
+    cy.get(isBilling ? '#w3w-billing' : '#w3w-shipping').clear();
+    cy.get(isBilling ? '#w3w-billing' : '#w3w-shipping').type(hint)
       .wait('@autosuggest')
       .get(isBilling ? '#w3w-billing' : '#w3w-shipping')
       .closest('what3words-autosuggest')
