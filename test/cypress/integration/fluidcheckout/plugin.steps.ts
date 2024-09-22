177a4a8e37996240d2a1d4d60f53f6defdc29460
Frederick Lee <frederickrichard.lee@gmail.com>
2024-09-22 23:01:49 +0100

    tests: updated cypress version

--- test/cypress/integration/fluidcheckout/plugin.steps.ts
+++ test/cypress/integration/fluidcheckout/plugin.steps.ts
@@ -5,7 +5,8 @@ import '../common/step_definitions/hooks.steps';
 const CH = new Chance();
 
 Before({ name: 'Enable fluid checkout plugin', tags: '@fluid_enabled' }, () =>
-  cy.installPlugin('fluid checkout', 'fluid-checkout')
+  cy
+    .installPlugin('fluid checkout', 'fluid-checkout')
     .activatePlugin('fluid-checkout')
 );
 
@@ -14,48 +15,62 @@ After({ name: 'Disable fluid checkout plugin', tags: '@fluid_enabled' }, () =>
 );
 
 Then('they see autosuggest when they open shipping', () => {
-  const fname = CH.first()
-  const lname = CH.last()
-  const add1 = CH.street()
-  const city = CH.city()
-  const postcode = CH.postcode()
-  const hint = 'filled.count.s'
+  const fname = CH.first();
+  const lname = CH.last();
+  const add1 = CH.street();
+  const city = CH.city();
+  const postcode = CH.postcode();
+  const hint = 'filled.count.s';
 
-  cy.intercept('http://localhost/?wc-ajax=update_order_review').as('update')
-  
-  cy.wait(500)
-  cy.get('#shipping_first_name').click().type(fname)
-  cy.get('#shipping_last_name').click().type(lname)
-  cy.get('#shipping_address_1').click().type(add1)
-  cy.get('#shipping_city').click().type(city)
-  cy.get('#shipping_postcode').click().type(postcode)
-  
+  cy.intercept('http://localhost:80/?wc-ajax=update_order_review').as('update');
+
+  cy.wait(500);
+  cy.get('#shipping_first_name').click().type(fname);
+  cy.get('#shipping_last_name').click().type(lname);
+  cy.get('#shipping_address_1').click().type(add1);
+  cy.get('#shipping_city').click().type(city);
+  cy.get('#shipping_postcode').click().type(postcode);
+
   // Start cruft - due to setTimeouts and other Fluid Checkout weirdness, this bit of cruft is necessary here
-  cy.wait(500)
+  cy.wait(500);
   cy.get(
     '#fc-expansible-form-section__toggle--shipping_w3w > .collapsible-content__inner > .expansible-section__toggle-plus'
-  ).focus().click()
-  cy.wait('@update')
+  )
+    .focus()
+    .click();
+  cy.wait('@update');
   cy.get(
     '#fc-expansible-form-section__toggle--shipping_w3w > .collapsible-content__inner > .expansible-section__toggle-plus'
-  ).focus().click()
-  cy.wait(500)
+  )
+    .focus()
+    .click();
+  cy.wait(500);
   // end cruft
-  
-  cy.get('#w3w-shipping').scrollIntoView().type(hint)
-  cy.get('[data-testid="suggestion-0"]').click()
-  cy.get('#w3w-shipping').invoke('val').should('match', /^\/\/\/(\w+.){2}\w+$/i)
-  cy.get('[data-step-id="shipping"] > .fc-step__actions > .fc-step__next-step').click()
-})
+
+  cy.get('#w3w-shipping').scrollIntoView().type(hint);
+  cy.get('[data-testid="suggestion-0"]').click();
+  cy.get('#w3w-shipping')
+    .invoke('val')
+    .should('match', /^\/\/\/(\w+.){2}\w+$/i);
+  cy.get(
+    '[data-step-id="shipping"] > .fc-step__actions > .fc-step__next-step'
+  ).click();
+});
 
 And('they see autosuggest when they open billing', () => {
-  const hint = 'm.a.s'
-  
-  cy.wait(200)
-  cy.get('#billing_same_as_shipping').click()
-  cy.get('#fc-expansible-form-section__toggle--billing_w3w > .collapsible-content__inner > .expansible-section__toggle-plus').scrollIntoView().click()
-  cy.get(`#w3w-billing`).focus().wait(300)
-  cy.get('#w3w-billing').click().type(hint, { delay: 300 })
-  cy.get('[data-testid="suggestion-0"]').click()
-  cy.get('#w3w-billing').invoke('val').should('match', /^\/\/\/(\w+.){2}\w+$/i)
-})
+  const hint = 'm.a.s';
+
+  cy.wait(200);
+  cy.get('#billing_same_as_shipping').click();
+  cy.get(
+    '#fc-expansible-form-section__toggle--billing_w3w > .collapsible-content__inner > .expansible-section__toggle-plus'
+  )
+    .scrollIntoView()
+    .click();
+  cy.get(`#w3w-billing`).focus().wait(300);
+  cy.get('#w3w-billing').click().type(hint, { delay: 300 });
+  cy.get('[data-testid="suggestion-0"]').click();
+  cy.get('#w3w-billing')
+    .invoke('val')
+    .should('match', /^\/\/\/(\w+.){2}\w+$/i);
+});
--- test/cypress/plugins/index.ts
+++ test/cypress/plugins/index.ts
@@ -1,6 +1,6 @@
 /// <reference type="cypress"
-const knex = require('knex')
-const Cypress = require('cypress')
+const knex = require('knex');
+const Cypress = require('cypress');
 const { default: cucumber } = require('cypress-cucumber-preprocessor');
 
 // ***********************************************************
@@ -33,7 +33,7 @@ module.exports = (on, config) => {
       database: config.env.DB_DATABASE,
     },
     pool: { min: 1, max: 2 },
-  })
+  });
 
   on('task', {
     'db:setup'() {
@@ -41,18 +41,23 @@ module.exports = (on, config) => {
         DB.table('wp_options')
           .where('option_name', 'w3w_autosuggest_settings')
           .update({
-            option_value: 'a:23:{s:7:"version";s:5:"4.0.0";s:7:"api_key";s:0:"";s:8:"selector";s:0:"";s:19:"woocommerce_enabled";b:1;s:18:"return_coordinates";b:0;s:18:"save_nearest_place";b:0;s:12:"enable_label";b:0;s:5:"label";s:0:"";s:18:"enable_placeholder";b:0;s:11:"placeholder";s:0:"";s:22:"enable_clip_to_country";b:0;s:15:"clip_to_country";s:0:"";s:21:"enable_clip_to_circle";b:0;s:18:"clip_to_circle_lat";s:0:"";s:18:"clip_to_circle_lng";s:0:"";s:21:"clip_to_circle_radius";s:0:"";s:27:"enable_clip_to_bounding_box";b:0;s:27:"clip_to_bounding_box_sw_lat";s:0:"";s:27:"clip_to_bounding_box_sw_lng";s:0:"";s:27:"clip_to_bounding_box_ne_lat";s:0:"";s:27:"clip_to_bounding_box_ne_lng";s:0:"";s:22:"enable_clip_to_polygon";b:0;s:15:"clip_to_polygon";s:0:"";}'
+            option_value:
+              'a:23:{s:7:"version";s:6:"4.0.15";s:7:"api_key";s:0:"";s:8:"selector";s:0:"";s:19:"woocommerce_enabled";b:1;s:18:"return_coordinates";b:0;s:18:"save_nearest_place";b:0;s:12:"enable_label";b:0;s:5:"label";s:0:"";s:18:"enable_placeholder";b:0;s:11:"placeholder";s:0:"";s:22:"enable_clip_to_country";b:0;s:15:"clip_to_country";s:0:"";s:21:"enable_clip_to_circle";b:0;s:18:"clip_to_circle_lat";s:0:"";s:18:"clip_to_circle_lng";s:0:"";s:21:"clip_to_circle_radius";s:0:"";s:27:"enable_clip_to_bounding_box";b:0;s:27:"clip_to_bounding_box_sw_lat";s:0:"";s:27:"clip_to_bounding_box_sw_lng";s:0:"";s:27:"clip_to_bounding_box_ne_lat";s:0:"";s:27:"clip_to_bounding_box_ne_lng";s:0:"";s:22:"enable_clip_to_polygon";b:0;s:15:"clip_to_polygon";s:0:"";}',
           }),
         DB.table('wp_options')
           .where('option_name', 'active_plugins')
-          .update({ option_value: 'a:1:{i:0;s:27:"woocommerce/woocommerce.php";}' }),
-        DB.table('wp_woocommerce_sessions')
-          .delete(),
-      ])
-    }
-  })
+          .update({
+            option_value: 'a:1:{i:0;s:27:"woocommerce/woocommerce.php";}',
+          }),
+        DB.table('wp_woocommerce_sessions').delete(),
+      ]);
+    },
+  });
 
-  on('file:preprocessor', cucumber({
-    typescript: require.resolve('typescript'),
-  }));
-}
+  on(
+    'file:preprocessor',
+    cucumber({
+      typescript: require.resolve('typescript'),
+    })
+  );
+};
