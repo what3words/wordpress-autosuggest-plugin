6acb4785a9fa8086f39580c3fd41a48d0865b4a5
chaddgrimm <chaddgrimm@users.noreply.github.com>
2025-06-26 13:01:20 +0100

    tests: updated manage spec

--- test/cypress/integration/public/managed.spec.ts
+++ test/cypress/integration/public/managed.spec.ts
@@ -34,8 +34,7 @@ describe('Managed fields', () => {
       beforeEach(() =>
         cy.visit('/shop')
           .get('[data-product_sku=woo-beanie]', { timeout: 10000 }).click({ force: true })
-          .visit('/cart')
-          .get('a[href*="checkout/"].checkout-button, a[href*="checkout/"].wc-block-cart__submit-button').click({ force: true })
+          .visit('/checkout')
       )
 
       it('Then the autosuggest search field is displayed', () => {
@@ -109,8 +108,8 @@ describe('Managed fields', () => {
         beforeEach(() =>
           cy.visit('/shop')
             .get('[data-product_sku=woo-beanie]', { timeout: 10000 }).click({ force: true })
-            .visit('/cart')
-            .get('a[href*="checkout/"].checkout-button, a[href*="checkout/"].wc-block-cart__submit-button').click({ force: true })
+            .visit('/checkout')
+
         )
 
         it('Then the component should load with clip to country configured', () => {
@@ -132,8 +131,7 @@ describe('Managed fields', () => {
       beforeEach(() =>
         cy.visit('/shop')
           .get('[data-product_sku=woo-beanie]', { timeout: 10000 }).click({ force: true })
-          .visit('/cart')
-          .get('a[href*="checkout/"].checkout-button, a[href*="checkout/"].wc-block-cart__submit-button').click({ force: true })
+          .visit('/checkout')
       )
 
       it('Then the autosuggest functionality is added to the existing field', () => {
@@ -220,8 +218,7 @@ describe('Managed fields', () => {
       beforeEach(() =>
         cy.visit('/shop')
           .get('[data-product_sku=woo-beanie]', { timeout: 10000 }).click({ force: true })
-          .visit('/cart')
-          .get('a[href*="checkout/"].checkout-button, a[href*="checkout/"].wc-block-cart__submit-button').click({ force: true })
+          .visit('/checkout')
       )
 
       it('Then the autosuggest functionality is added to the existing field', () => {
